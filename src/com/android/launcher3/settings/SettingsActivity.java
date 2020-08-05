b5bf929a36a1d43fd966369464b6a04afa0deca5
Joey <joey@lineageos.org>
2020-08-05 15:52:24 +0200

    Launcher3: Add support for icon packs
    - Supports icon packs from nova and adw
    - Live preview in settings
    - Default applied icon pack via overlay
    
    Change-Id: Ie9879fa30f4cd9d52356acf78a423b415657510c
    Signed-off-by: Adithya R <gh0strider.2k18.reborn@gmail.com>

--- src/com/android/launcher3/settings/SettingsActivity.java
+++ src/com/android/launcher3/settings/SettingsActivity.java
@@ -25,7 +25,10 @@ import static com.android.launcher3.util.SecureSettingsObserver.newNotificationS
 
 import static com.corvus.launcher.OverlayCallbackImpl.KEY_ENABLE_MINUS_ONE;
 
+import android.app.Activity;
+import android.app.ActivityManager;
 import android.content.Context;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.Intent;
 import android.os.Bundle;
@@ -54,6 +57,8 @@ import com.android.launcher3.Utilities;
 import com.android.launcher3.config.FeatureFlags;
 import com.android.launcher3.LauncherTab;
 import com.android.launcher3.trust.TrustAppsActivity;
+import com.android.launcher3.corvus.icon.IconPackStore;
+import com.android.launcher3.corvus.icon.IconPackSettingsActivity;
 import com.android.launcher3.uioverrides.plugins.PluginManagerWrapper;
 import com.android.launcher3.util.SecureSettingsObserver;
 
@@ -79,6 +84,7 @@ public class SettingsActivity extends FragmentActivity
 
     public static boolean restartNeeded = false;
     public static final String KEY_TRUST_APPS = "pref_trust_apps";
+    public static final String KEY_ICON_PACK = "pref_icon_pack";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -159,7 +165,8 @@ public class SettingsActivity extends FragmentActivity
     /**
      * This fragment shows the launcher preferences.
      */
-    public static class LauncherSettingsFragment extends PreferenceFragmentCompat {
+    public static class LauncherSettingsFragment extends PreferenceFragmentCompat implements
+            SharedPreferences.OnSharedPreferenceChangeListener {
 
         private SecureSettingsObserver mNotificationDotsObserver;
 
@@ -186,6 +193,20 @@ public class SettingsActivity extends FragmentActivity
             getPreferenceManager().setSharedPreferencesName(LauncherFiles.SHARED_PREFERENCES_KEY);
             setPreferencesFromResource(R.xml.launcher_preferences, rootKey);
 
+            updatePreferences();
+
+            Utilities.getPrefs(getContext())
+                    .registerOnSharedPreferenceChangeListener(this);
+        }
+
+        @Override
+        public void onDestroyView () {
+            Utilities.getPrefs(getContext())
+                .unregisterOnSharedPreferenceChangeListener(this);
+            super.onDestroyView();
+        }
+
+        private void updatePreferences() {
             PreferenceScreen screen = getPreferenceScreen();
             for (int i = screen.getPreferenceCount() - 1; i >= 0; i--) {
                 Preference preference = screen.getPreference(i);
@@ -205,6 +226,15 @@ public class SettingsActivity extends FragmentActivity
         public void onSaveInstanceState(Bundle outState) {
             super.onSaveInstanceState(outState);
             outState.putBoolean(SAVE_HIGHLIGHTED_KEY, mPreferenceHighlighted);
+        }
+
+        @Override
+        public void onSharedPreferenceChanged(SharedPreferences prefs, String key) {
+            switch (key) {
+                case IconPackStore.KEY_ICON_PACK:
+                    updatePreferences();
+                    break;
+            }
         }
 
         protected String getParentKeyForPref(String key) {
@@ -262,6 +292,10 @@ public class SettingsActivity extends FragmentActivity
                 case KEY_ENABLE_MINUS_ONE:
                     mShowGoogleAppPref = preference;
                     updateIsGoogleAppEnabled();
+                    return true;
+
+		case KEY_ICON_PACK:
+                    setupIconPackPreference(preference);
                     return true;
 
                 case KEY_TRUST_APPS:
@@ -351,6 +385,17 @@ public class SettingsActivity extends FragmentActivity
             // then we must force a restart because the home button watcher wouldn't trigger it
             LauncherAppState.getInstanceNoCreate().checkIfRestartNeeded();
             super.onDestroy();
+        }
+
+        private void setupIconPackPreference(Preference preference) {
+            final Context context = getContext();
+            final String defaultLabel = context.getString(R.string.icon_pack_default_label);
+            final String pkgLabel = new IconPackStore(context).getCurrentLabel(defaultLabel);
+            preference.setSummary(pkgLabel);
+            preference.setOnPreferenceClickListener(p -> {
+                startActivity(new Intent(getActivity(), IconPackSettingsActivity.class));
+                return true;
+            });
         }
     }
 }
