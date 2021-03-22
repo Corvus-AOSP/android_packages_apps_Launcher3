bb4a16cab4e80f2c9f7716d76a62eeb1b26c74db
DennySPb <dennyspb@gmail.com>
2021-03-22 16:13:47 +0300

    Launcher: Remove Protected apps in favour of system AppLock
    Signed-off-by: DennySPb <dennyspb@gmail.com>
    Change-Id: Ie785da5f44389a87cb2a8a42a8e40f817fc72228

--- src/com/android/launcher3/settings/SettingsActivity.java
+++ src/com/android/launcher3/settings/SettingsActivity.java
@@ -300,11 +300,8 @@ public class SettingsActivity extends FragmentActivity
 
                 case KEY_TRUST_APPS:
                     preference.setOnPreferenceClickListener(p -> {
-                        Utilities.showLockScreen(getActivity(),
-                                getString(R.string.trust_apps_manager_name), () -> {
-                            Intent intent = new Intent(getActivity(), TrustAppsActivity.class);
-                            startActivity(intent);
-                        });
+                        Intent intent = new Intent(getActivity(), TrustAppsActivity.class);
+                        startActivity(intent);
                         return true;
                     });
             }
