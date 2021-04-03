Wave-Project/packages_apps_Launcher3

Launcher3: Use our custom icon factory to decide icon wrapping
Change-Id: Ie287c918106e786cf6a2d77ca7188a2aa7556fa2
Signed-off-by: Adithya R <gh0strider.2k18.reborn@gmail.com>

src/com/android/launcher3/graphics/LauncherPreviewRenderer.java
@@ -65,10 +65,10 @@ import com.android.launcher3.R;
 import com.android.launcher3.WorkspaceLayoutManager;
 import com.android.launcher3.config.FeatureFlags;
 import com.android.launcher3.folder.FolderIcon;
-import com.android.launcher3.icons.BaseIconFactory;
 import com.android.launcher3.icons.BitmapInfo;
 import com.android.launcher3.icons.IconProvider;
 import com.android.launcher3.icons.LauncherIcons;
+import com.android.launcher3.wave.icon.WaveIconFactory;
 import com.android.launcher3.model.AllAppsList;
 import com.android.launcher3.model.BgDataModel;
 import com.android.launcher3.model.BgDataModel.Callbacks;
@@ -227,8 +227,8 @@ public class LauncherPreviewRenderer {
         mInsets.top = mInsets.bottom = (mDp.heightPx - mDp.availableHeightPx) / 2;
         mDp.updateInsets(mInsets);
 
-        BaseIconFactory iconFactory =
-                new BaseIconFactory(context, mIdp.fillResIconDpi, mIdp.iconBitmapSize) { };
+        WaveIconFactory iconFactory =
+                new WaveIconFactory(context, mIdp.fillResIconDpi, mIdp.iconBitmapSize) { };
         BitmapInfo iconInfo = iconFactory.createBadgedIconBitmap(new AdaptiveIconDrawable(
                         new ColorDrawable(Color.WHITE), new ColorDrawable(Color.WHITE)),
                 Process.myUserHandle(),

src/com/android/launcher3/icons/LauncherIcons.java
@@ -21,12 +21,13 @@ import android.content.Context;
 import com.android.launcher3.InvariantDeviceProfile;
 import com.android.launcher3.graphics.IconShape;
 import com.android.launcher3.graphics.LauncherPreviewRenderer;
+import com.android.launcher3.wave.icon.WaveIconFactory;
 
 /**
  * Wrapper class to provide access to {@link BaseIconFactory} and also to provide pool of this class
  * that are threadsafe.
  */
-public class LauncherIcons extends BaseIconFactory implements AutoCloseable {
+public class LauncherIcons extends WaveIconFactory implements AutoCloseable {
 
     private static final Object sPoolSync = new Object();
     private static LauncherIcons sPool;

Corvus-AOSP/android_packages_apps_Launcher3

src/com/android/launcher3/graphics/LauncherPreviewRenderer.java
@@ -65,9 +65,9 @@ import com.android.launcher3.R;
 import com.android.launcher3.WorkspaceLayoutManager;
 import com.android.launcher3.config.FeatureFlags;
 import com.android.launcher3.folder.FolderIcon;
-import com.android.launcher3.icons.BaseIconFactory;
 import com.android.launcher3.icons.BitmapInfo;
 import com.android.launcher3.icons.LauncherIcons;
+import com.android.launcher3.corvus.icon.CorvusIconFactory;
 import com.android.launcher3.model.AllAppsList;
 import com.android.launcher3.model.BgDataModel;
 import com.android.launcher3.model.BgDataModel.Callbacks;
@@ -226,8 +226,8 @@ public class LauncherPreviewRenderer {
         mInsets.top = mInsets.bottom = (mDp.heightPx - mDp.availableHeightPx) / 2;
         mDp.updateInsets(mInsets);
 
-        BaseIconFactory iconFactory =
-                new BaseIconFactory(context, mIdp.fillResIconDpi, mIdp.iconBitmapSize) { };
+        CorvusIconFactory iconFactory =
+                new CorvusIconFactory(context, mIdp.fillResIconDpi, mIdp.iconBitmapSize) { };
         BitmapInfo iconInfo = iconFactory.createBadgedIconBitmap(new AdaptiveIconDrawable(
                         new ColorDrawable(Color.WHITE), new ColorDrawable(Color.WHITE)),
                 Process.myUserHandle(),

src/com/android/launcher3/icons/LauncherIcons.java
@@ -21,12 +21,13 @@ import android.content.Context;
 import com.android.launcher3.InvariantDeviceProfile;
 import com.android.launcher3.graphics.IconShape;
 import com.android.launcher3.graphics.LauncherPreviewRenderer;
+import com.android.launcher3.corvus.icon.CorvusIconFactory;
 
 /**
  * Wrapper class to provide access to {@link BaseIconFactory} and also to provide pool of this class
  * that are threadsafe.
  */
-public class LauncherIcons extends BaseIconFactory implements AutoCloseable {
+public class LauncherIcons extends CorvusIconFactory implements AutoCloseable {
 
     private static final Object sPoolSync = new Object();
     private static LauncherIcons sPool;