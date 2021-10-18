4d411b3bf00f65b6fa13226c8d94230c51b55ad4
Suphon Thanakornpakapong <paphonb@gmail.com>
2021-10-18 12:06:07 +0700

    Launcher3: Add Google Lens scan button to recents
    And a squash of:
    
    Launcher3: Restart launcher on changing google search/lens app
    
    Launcher3: Make lens available without lens package
    
    * Google search package is capable of lens functionality. Who knew? :)
    
    idoybh (YAAP): Keep clear all rightmost
    
    Co-authored-by: Pranav Vashi <neobuddy89@gmail.com>
    Co-authored-by: hundeva <hundeva@gmail.com>
    Signed-off-by: Pranav <npv12@iitbbs.ac.in>

--- quickstep/src/com/android/quickstep/TaskOverlayFactory.java
+++ quickstep/src/com/android/quickstep/TaskOverlayFactory.java
@@ -351,6 +351,14 @@ public class TaskOverlayFactory implements ResourceBasedOverride {
             public void onSplit() {
                 endLiveTileMode(TaskOverlay.this::enterSplitSelect);
             }
+
+            public void onLens() {
+                if (mIsAllowedByPolicy) {
+                    endLiveTileMode(() -> mImageApi.startLensActivity());
+                } else {
+                    showBlockedByPolicyMessage();
+                }
+            }
         }
     }
 
@@ -364,5 +372,7 @@ public class TaskOverlayFactory implements ResourceBasedOverride {
 
         /** User wants to start split screen with current app. */
         void onSplit();
+
+        void onLens();
     }
 }
