b4c1ba92192acaf7190eff7d7fe32d9e0930fc1a
Danny Lin <danny@kdrag0n.dev>
2022-04-10 20:54:32 -0700

    Allow enabling recents overview grid on phones
    Change-Id: I610988f90d249c0c9284d89a160f2e0bf2f0b154

--- quickstep/src/com/android/quickstep/views/RecentsView.java
+++ quickstep/src/com/android/quickstep/views/RecentsView.java
@@ -3270,7 +3270,9 @@ public abstract class RecentsView<ACTIVITY_TYPE extends StatefulActivity<STATE_T
             return;
         }
         mActionsView.setSplitButtonVisible(
-                mActivity.getDeviceProfile().overviewShowAsGrid && getTaskViewCount() > 1);
+                mActivity.getDeviceProfile().overviewShowAsGrid &&
+                        mActivity.getDeviceProfile().isTablet &&
+                        getTaskViewCount() > 1);
     }
 
     /**
